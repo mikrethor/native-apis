8df732e757bca8fea7e4f760f04c13ce22c138c6
Xavier Bouclet <mikrethor@gmail.com>
2023-02-22 08:30:22 -0500

    Add test Junit4

---
+++ native-spring-api/src/test/java/com/xavierbouclet/nativeapi/ActorServiceTest.java
@@ -0,0 +1,39 @@
+package com.xavierbouclet.nativeapi;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.List;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ActorServiceTest {
+     public ActorService actorService;
+
+
+    @MockBean
+    private ActorRepository actorRepository;
+    @MockBean
+    private MovieRepository movieRepository;
+
+    @Before
+    public void before() {
+        actorService = new ActorService(actorRepository, movieRepository);
+    }
+
+    @Test
+    public void list() {
+        var actor=mock(Actor.class);
+        List<Actor> actors = List.of(actor);
+        when(actorRepository.findAll()).thenReturn(actors);
+        actorService = new ActorService(actorRepository, movieRepository);
+        assertThat(actorService.list()).isEqualTo(actors);
+    }
+}
